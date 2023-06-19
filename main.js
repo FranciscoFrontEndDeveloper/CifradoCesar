f1adc6c6a5e0abc284b934963c6e0911e396afa6
FRANCISCO <pachonetworking@gmail.com>
2023-06-19 16:04:41 -0500

    rot13 object created

---
+++ main.js
@@ -0,0 +1,36 @@
+function rot13(str) {
+  const objetRot13 = {
+    A: 'N',
+    B: 'O',
+    C: 'P',
+    D: 'Q',
+    E: 'R',
+    F: 'S',
+    G: 'T',
+    H: 'U',
+    I: 'V',
+    J: 'W',
+    K: 'X',
+    L: 'Y',
+    M: 'Z',
+    N: 'A',
+    O: 'B',
+    P: 'C',
+    Q: 'D',
+    R: 'E',
+    S: 'F',
+    T: 'G',
+    U: 'H',
+    V: 'I',
+    W: 'J',
+    X: 'K',
+    Y: 'L',
+    Z: 'M'
+ } 
+ 
+  console.log(objetRot13)
+
+  return str;
+}
+
+rot13("SERR PBQR PNZC");
